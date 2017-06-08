6c329ab8fe61a166ddd35bd8d7bc0de2764c8372
sadhch <sadhana.chandravanka@feisystems.com>
2017-06-08 14:36:45 -0400

    Add C2S V3 related end points
    #14009 - As a developer I will have implemented backend to support - Segmentation/Upload/download patient health record workflow

--- pep/src/main/java/gov/samhsa/c2s/pep/web/PolicyEnforcementPointRestController.java
+++ pep/src/main/java/gov/samhsa/c2s/pep/web/PolicyEnforcementPointRestController.java
@@ -22,18 +22,8 @@ public class PolicyEnforcementPointRestController {
     private PolicyEnforcementPoint policyEnforcementPoint;
 
     @RequestMapping(value = "/access", method = RequestMethod.POST)
-    public AccessResponseDto access(@Valid @RequestBody AccessRequestDto accessRequest, Principal principal) {
-        logPrincipal(principal);
+    public AccessResponseDto access(@Valid @RequestBody AccessRequestDto accessRequest) {
         return policyEnforcementPoint.accessDocument(accessRequest);
     }
 
-    @RequestMapping(value = "/documents", method = RequestMethod.GET)
-    public SegmentedDocumentsResponseDto getDocuments(@RequestParam("mrn") String mrn, @RequestParam("purposeOfUse") String purposeOfUse, @RequestParam("domain") String domain, Principal principal) {
-        logPrincipal(principal);
-        return policyEnforcementPoint.getCCDDocuments(principal.getName(), mrn, purposeOfUse, domain);
-    }
-
-    private void logPrincipal(Principal principal) {
-        logger.debug(() -> "Handling request with principal: " + Optional.ofNullable(principal).map(Principal::getName).orElse("Principal.getName() not found"));
-    }
 }
