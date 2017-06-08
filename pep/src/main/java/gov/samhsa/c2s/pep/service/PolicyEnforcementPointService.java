b50b6be8705dee13bee23a0621775a9896d37135
sadhch <sadhana.chandravanka@feisystems.com>
2017-06-08 15:52:02 -0400

    Remove unused dtos as part of refactoring the core service
    #14009 - As a developer I will have implemented backend to support - Segmentation/Upload/download patient health record workflow

--- pep/src/main/java/gov/samhsa/c2s/pep/service/PolicyEnforcementPointService.java
+++ pep/src/main/java/gov/samhsa/c2s/pep/service/PolicyEnforcementPointService.java
@@ -2,7 +2,6 @@ package gov.samhsa.c2s.pep.service;
 
 import gov.samhsa.c2s.pep.service.dto.AccessRequestDto;
 import gov.samhsa.c2s.pep.service.dto.AccessResponseDto;
-import gov.samhsa.c2s.pep.service.dto.SegmentedDocumentsResponseDto;
 
 public interface PolicyEnforcementPointService {
 
